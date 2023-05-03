7696e48c1d841e4078c724e5e116631169e2961a
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-03 15:38:53 +0700

    add book.go\usecase

---
+++ task/usecase/book.go
@@ -0,0 +1,65 @@
+package usecase
+
+import (
+	"errors"
+	"fmt"
+	"task/lib/database"
+	"task/models"
+)
+
+func CreateBook(book *models.Book) error {
+	// check title cannot be empty
+	if book.Title == "" {
+		return errors.New("book title cannot be empty")
+	}
+
+	//check creator
+	if book.Author == "" {
+		return errors.New("book Author cannot be empty")
+	}
+
+	err := database.CreateBook(book)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetBook(id uint) (book models.Book, err error) {
+	book, err = database.GetBook(id)
+	if err != nil {
+		fmt.Println("Error getting book from database")
+		return
+	}
+	return
+}
+
+func GetListBooks() (users []models.Book, err error) {
+	if err != nil {
+		fmt.Println("GetListBook : Error getting user from database")
+		return
+	}
+	return
+}
+
+func UpdateBook(book *models.Book) (err error) {
+	err = database.UpdateBook(book)
+	if err != nil {
+		fmt.Println("UpdateBook : Error Updating Book")
+		return
+	}
+
+	return
+}
+
+func DeleteBook(id uint) (err error) {
+	book := models.Book{}
+	book.ID = id
+	err = database.DeleteBook(&book)
+	if err != nil {
+		fmt.Println("DeleteBook : error deleting book, err ", err)
+		return
+	}
+
+	return
+}
