35fd44103e1dae54a8276a24e071748d2e57aea7
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-03 15:39:17 +0700

    add book.go\database

---
+++ task/lib/database/book.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"task/config"
+	"task/models"
+)
+
+func CreateBook(book *models.Book) error {
+	if err := config.DB.Save(book).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetBooks() (interface{}, error) {
+	var books []models.Book
+
+	if err := config.DB.Find(&books).Error; err != nil {
+		return nil, err
+	}
+	return books, nil
+}
+
+func GetBook(id uint) (book models.Book, err error) {
+	book.ID = id
+	if err = config.DB.First(&book).Error; err != nil {
+		return
+	}
+	return
+}
+
+func UpdateBook(book *models.Book) error {
+	if err := config.DB.Updates(book).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteBook(book *models.Book) error {
+	if err := config.DB.Delete(book).Error; err != nil {
+		return err
+	}
+	return nil
+}
