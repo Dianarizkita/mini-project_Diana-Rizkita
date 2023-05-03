80bc87870480a74b893f176a1545d8f485d6f925
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-03 15:37:25 +0700

    add book.go\models

---
+++ task/models/book.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Title           string `json:"title" form:"title"`
+	Author          string `json:"author" form:"author"`
+	Publisher       string `json:"publisher" form:"publisher"`
+	PublicationYear string `json:"publication_year" form:"publication_year"`
+	Genre           string `json:"genre" form:"genre"`
+	BookStock       int32  `json:"book_stock" form:"book_stock"`
+}
