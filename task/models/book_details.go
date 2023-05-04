6e8b6c4ee4085cd7d71ee77416c3b5ef906e89fd
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-04 17:47:29 +0700

    Edit book_details\models

--- task/models/book_details.go
+++ task/models/book_details.go
@@ -7,6 +7,6 @@ import (
 type Book_Details struct {
 	gorm.Model
 	BookCode string `json:"book_code" form:"book_code"`
-	Book     []Book `json:"book" form:"book"`
 	Status   string `json:"status" form:"status"`
+	Book     []Book `json:"book" form:"book"`
 }
