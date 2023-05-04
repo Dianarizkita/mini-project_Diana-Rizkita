b9ec17a7a81a5caaf966757319b3226d1d737dbc
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-04 17:57:13 +0700

    add user\database

--- task/lib/database/user.go
+++ task/lib/database/user.go
@@ -15,28 +15,27 @@ func CreateUser(user *models.User) error {
 func GetUsers() (interface{}, error) {
 	var users []models.User
 
-	if err := config.DB.Find(&users).Error; err != nil {
+	if err := config.DB.Model(&models.User{}).Preload("Blogs").Find(&users).Error; err != nil {
 		return nil, err
 	}
 	return users, nil
 }
 
-func GetUser(user *models.User) (err error) {
-
-	if err = config.DB.First(&user).Error; err != nil {
-		return
-	}
-	return
-}
-
-func GetUserWithBlog(id uint) (user models.User, err error) {
+func GetUser(id uint) (user models.User, err error) {
 	user.ID = id
-	if err = config.DB.First(&user).Error; err != nil {
+	if err = config.DB.Model(&models.User{}).Preload("Blogs").First(&user).Error; err != nil {
 		return
 	}
 	return
 }
 
+//	func UpdateUser(id uint) (user models.User, err error) {
+//		user.ID = id
+//		if err = config.DB.Updates(&user).Error; err != nil {
+//			return
+//		}
+//		return
+//	}
 func UpdateUser(user *models.User) error {
 	if err := config.DB.Updates(user).Error; err != nil {
 		return err
