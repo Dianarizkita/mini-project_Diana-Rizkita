b97e7cf04471268ba864b536277c24315a38f3a9
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-08 15:14:02 +0700

    upadate book details

--- task/models/book_details.go
+++ task/models/book_details.go
@@ -6,7 +6,8 @@ import (
 
 type Book_Details struct {
 	gorm.Model
-	BookID   uint   `json:"book_id" form:"book_id"`
-	BookCode string `json:"book_code" form:"book_code"`
-	Status   string `json:"status" form:"status"`
+	BookID       uint   `json:"book_id" form:"book_id"`
+	BookCode     string `json:"book_code" form:"book_code"`
+	Status       string `json:"status" form:"status"`
+	Transactions []Transaction
 }
