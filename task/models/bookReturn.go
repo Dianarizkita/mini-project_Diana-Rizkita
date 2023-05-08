77ddcc833faf02b21486cef9d6eaad8f79a39513
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-08 15:14:14 +0700

    upadate book return

--- task/models/bookReturn.go
+++ task/models/bookReturn.go
@@ -1,11 +1,14 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Book_Return struct {
 	gorm.Model
-	DateOfReturn string `json:"date_of_return" form:"date_of_return"`
-	Penalty      string `json:"penalty" form:"penalty"`
+	TransactionID uint      `json:"transaction_id" form:"transaction_id"`
+	DateOfReturn  time.Time `json:"date_of_return"`
+	Penalty       string    `json:"penalty" form:"penalty"`
 }
