f75b1a3888331eb8510f93438dad7f5431190008
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-09 17:44:53 +0700

    Update book return

--- task/models/bookReturn.go
+++ task/models/bookReturn.go
@@ -6,6 +6,7 @@ import (
 
 type Book_Return struct {
 	gorm.Model
-	DateOfReturn string `json:"date_of_return" form:"date_of_return"`
-	Penalty      string `json:"penalty" form:"penalty"`
+	TransactionID uint   `json:"transaction_id" form:"transaction_id"`
+	DateOfReturn  string `json:"date_of_return" form:"date_of_return"`
+	Penalty       string `json:"penalty" form:"penalty"`
 }
