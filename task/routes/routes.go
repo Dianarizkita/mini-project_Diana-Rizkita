f5d7b6510c99eb7e5f7b4b45403d8c06e7593cfc
Dianarizkita <110362567+Dianarizkita@users.noreply.github.com>
2023-05-09 17:45:11 +0700

    Update route

--- task/routes/routes.go
+++ task/routes/routes.go
@@ -37,5 +37,28 @@ func New() *echo.Echo {
 	book_details.PUT(":id", controllers.UpdateBookDetailsController)
 	book_details.DELETE(":id", controllers.DeleteBookDetailsController)
 
+	//route book details
+	book_return := e.Group("/book_return")
+	book_return.GET("", controllers.GetReturnControllers)
+	book_return.POST("", controllers.CreateReturnController)
+	book_return.GET(":id", controllers.GetReturnController)
+	book_return.PUT(":id", controllers.UpdateReturnController)
+	book_return.DELETE(":id", controllers.DeleteReturnController)
+
+	//route user
+	user := e.Group("/users")
+	user.GET("", controllers.GetUserControllers)
+	user.POST("", controllers.CreateUserController)
+	user.GET(":id", controllers.GetUserController)
+	user.PUT(":id", controllers.UpdateUserController)
+	user.DELETE(":id", controllers.DeleteUserController)
+
+	//route transaction
+	transaction := e.Group("/transactions")
+	transaction.GET("", controllers.GetTransactionControllers)
+	transaction.POST("", controllers.CreateTransactionController)
+	transaction.GET(":id", controllers.GetTransactionController)
+	transaction.PUT(":id", controllers.UpdateTransactionController)
+	transaction.DELETE(":id", controllers.DeleteTransactionController)
 	return e
 }
